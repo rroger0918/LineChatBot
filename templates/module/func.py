7dd1c338fd6b2084294781469d1e2ce4cde7c83b
Jayce0918 <51176006+Jayce0918@users.noreply.github.com>
2022-03-13 20:01:27 +0800

    2022-03-13

--- templates/module/func.py
+++ templates/module/func.py
@@ -471,6 +471,10 @@ def error(event):  # 傳送錯誤文字
                         action=MessageAction(
                             label="黑狗軒免運布朗尼?", text="我想買黑狗軒免運布朗尼")
                     ),
+                    QuickReplyButton(
+                        action=MessageAction(
+                            label="回憶相簿?", text="開啟相簿")
+                    ),
                     # QuickReplyButton(
                     #     action=MessageAction(
                     #         label="周邊網頁?", text="周邊網頁介紹")
